629054fadf8fdd41bd452c75aeb76a6caab4bba9
Felix Breidenstein <felix@codemonauts.com>
2021-07-12 12:22:33 +0200

    Init sourcemaps at the right place

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ function script(scriptpath, destination = '../public/js') {
             log.error("Browserify Error: " + err.message);
         })
         .pipe(source(scriptpath.split("/").pop()))
-        .pipe($.if(isProduction(), $.sourcemaps.init()))
         .pipe(buffer())
+        .pipe($.if(isProduction(), $.sourcemaps.init()))
         .pipe($.if(isProduction(), uglify()))
         .pipe($.if(isProduction(), $.sourcemaps.write('./maps')))
         .pipe(dest(destination));
